b8d329ac698cc0678331b4e10577f2a337781180
Daniel Burnley <dan@danops.uk>
2018-01-05 16:14:24 +0000

    add example test for render for next week so we know what were doing

--- test/createElement.test.js
+++ test/createElement.test.js
@@ -12,4 +12,9 @@ describe("createElement", () => {
   it('creates an object with children', () => {
     expect(createElement("div", {}, ['waffles'])).toEqual({type: "div", props: {}, children: ['waffles']})
   })
+
+  it("does some shit", () => {
+    let res = document.createElement("div")
+    expect("<div />").toEqual(res)
+  })
 })
