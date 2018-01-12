fd6519930d8fd1cd0d571fd79f0d55a5117d8102
Daniel Burnley <dan@danops.uk>
2018-01-12 13:54:08 +0000

    remove temp test

--- test/createElement.test.js
+++ test/createElement.test.js
@@ -12,9 +12,4 @@ describe("createElement", () => {
   it('creates an object with children', () => {
     expect(createElement("div", {}, ['waffles'])).toEqual({type: "div", props: {}, children: ['waffles']})
   })
-
-  it("does some shit", () => {
-    let res = document.createElement("div")
-    expect("<div />").toEqual(res)
-  })
 })
