dc5377d81aec4b7dc28190a332f45449db263a52
Daniel Burnley <dan@danops.uk>
2018-01-26 14:47:53 +0000

    Finish render basic elemnet tests

--- waff/renderElements.js
+++ waff/renderElements.js
@@ -4,18 +4,28 @@ function renderElementWithChildren(element) {
   for (let child of element.children) {
     childString += renderElements(child)
   }
-  return `<${type}>${childString}</${type}>`
+
+  let propString = generatePropString(element.props)
+
+  return `<${type}${propString}>${childString}</${type}>`
 }
 
 function renderElementWithoutChildren(element) {
   let type = element.type
+
+  let propString = generatePropString(element.props)
+
+  return `<${type}${propString} />`
+}
+
+function generatePropString(props) {
   let propString = ""
 
-  for (let prop in element.props) {
-    propString += `${[prop]}='${element.props[prop]}' `
+  for (let prop in props) {
+    propString += ` ${[prop]}='${props[prop]}'`
   }
 
-  return `<${type} ${propString}/>`
+  return propString
 }
 
 export default function renderElements(element) {
